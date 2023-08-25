f6737be1e0a56a967f0445fafa7126af1c8b2cce
Mateus Juvencio <mateus.juvencio@axxindata.com>
2023-08-24 21:40:45 -0300

    feat: adicionando efeitos e melhorias na side bar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,4 +1,4 @@
-import { SideBarContainer, UlContainer, LiContainer, ImgContainer,SpanContainer } from "./styled";
+import { SideBarContainer, UlContainer, LiContainer, ImgContainer,SpanContainer, HrContainer } from "./styled";
 import fogueteIcon from "../../assets/foguete.png"
 import foguetevoando from "../../assets/foguetevoando.png"
 import info from "../../assets/info.png"
@@ -6,16 +6,16 @@ import info from "../../assets/info.png"
 export default function SideBar(){
      return (
           <SideBarContainer>
-               <UlContainer >
-                    <LiContainer className="li">
-                         <ImgContainer src={foguetevoando} className="foguete-logo" alt="foguete" />
+               <UlContainer>
+                    <LiContainer className="active">
+                         <ImgContainer src={foguetevoando} className="active" alt="foguete" />
                          <SpanContainer className="sp">Lançamentos</SpanContainer>
                     </LiContainer>
-                    <LiContainer>
+                    <LiContainer className="">
                          <ImgContainer src={fogueteIcon} className="foguete-logo" alt="foguete" />
                          <SpanContainer className="sp">Foguetes</SpanContainer>
                     </LiContainer>
-                    <LiContainer>
+                    <LiContainer className="">
                          <ImgContainer src={info} className="foguete-logo" alt="foguete" />
                          <SpanContainer className="sp">Informações</SpanContainer>
                     </LiContainer>
--- src/components/SideBar/styled.tsx
+++ src/components/SideBar/styled.tsx
@@ -2,11 +2,19 @@ import styled from 'styled-components';
 
 export const SideBarContainer = styled.div`
      display: flex;
-     flex: 0 0 250px; /* Define a largura da barra lateral */
+     flex: 0 0 200px; /* Define a largura da barra lateral */
      align-items: center;
      color: white;
-     padding: 20px;
+     padding: 0px;
+     font-family: sans;
+
+     font-weight: bold;
 `;
+
+export const HrContainer = styled.hr`
+     whidth:100px;
+     color: black;
+`
 
 export const UlContainer = styled.ul`
      display: flex;
@@ -14,35 +22,39 @@ export const UlContainer = styled.ul`
      align-items: start;
      text-decoration: none;
      list-style: none;
-     padding: 0;
+     padding-top: 60px;
 `;
 export const LiContainer = styled.li`
+     background-color: ${(props) => props.className === "active" ? 'white' : ''};
+     border-radius: ${(props) => props.className === "active" ? '0 30px 30px 0px' : ''};
+     .sp {
+          display: ${(props) => props.className === "active" ? 'none' : ''};;
+     }
      cursor: pointer;
      display: flex;
      align-items: center;
-     padding: 15px;
+     padding: 20px;
+     margin: 10px;
      &:hover {
           .sp {
                opacity:1;
           }
           background-color: white;
-          border-radius: 50px;
-          transition: all 0.5s ease-in-out;
-          margin: 10px;
+          transition: all 0.5s ease;
+          border-radius: 0 30px 30px 0px;
           padding: 20px;
+          margin: 10px;
      }
 `;
-
-
 export const ImgContainer = styled.img`
      width: 80px;
      height: 80px;
 `;
-
 export const SpanContainer = styled.span`
-     
      opacity: 0;
      color: #282c34;
      transition: opacity 0.3s ease;
      
 `;
+
+
